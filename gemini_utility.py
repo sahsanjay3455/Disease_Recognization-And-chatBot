637e7182d944419fed49e3239cb4f74393b18ca0
Sanjay Sah <138980970+sahsanjay3455@users.noreply.github.com>
2024-09-24 10:14:48 +0530

    gemini_utility.py

--- gemini_utility.py
+++ gemini_utility.py
@@ -1,10 +1,7 @@
 import os
 import json
-from PIL import Image
-
-
-
 import google.generativeai as genai
+
 # working directory path
 working_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -19,13 +16,13 @@ GOOGLE_API_KEY = config_data["GOOGLE_API_KEY"]
 genai.configure(api_key=GOOGLE_API_KEY)
 
 def load_gemini_pro_model():
-    gemini_pro_model = genai.GenerativeModel("gemini-pro")
+    gemini_pro_model = genai.GenerativeModel("gemini-1.5-flash")
     return gemini_pro_model
 
 
 # get response from Gemini-Pro-Vision model - image/text to text
 def gemini_pro_vision_response(prompt, image):
-    gemini_pro_vision_model = genai.GenerativeModel("gemini-pro-vision")
+    gemini_pro_vision_model = genai.GenerativeModel("gemini-1.5-flash")
     response = gemini_pro_vision_model.generate_content([prompt, image])
     result = response.text
     return result
@@ -35,18 +32,15 @@ def gemini_pro_vision_response(prompt, image):
 def embeddings_model_response(input_text):
     embedding_model = "models/embedding-001"
     embedding = genai.embed_content(model=embedding_model,
-                                    content=input_text,
-                                    task_type="retrieval_document")
+                                     content=input_text,
+                                     task_type="retrieval_document")
     embedding_list = embedding["embedding"]
     return embedding_list
 
 
 # get response from Gemini-Pro model - text to text
 def gemini_pro_response(user_prompt):
-    gemini_pro_model = genai.GenerativeModel("gemini-pro")
+    gemini_pro_model = genai.GenerativeModel("gemini-1.5-flash")
     response = gemini_pro_model.generate_content(user_prompt)
     result = response.text
     return result
-
-
-
